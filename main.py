10d349f9d5d02a26299d0a28c36b5a6c4a9aeff3
ryanprogrammer9 <ryan_programmer@outlook.com>
2024-07-29 00:24:21 +0330

    initial bot

---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+import logging
+
+from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        "Please press the button below to choose a color via the WebApp.",
+        reply_markup=ReplyKeyboardMarkup.from_button(
+            KeyboardButton(
+                text="Open the color picker!",
+                web_app=WebAppInfo(url="https://msn.com"),
+            )
+        ),
+    )
+
+async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Print the received data and remove the button."""
+    # Here we use `json.loads`, since the WebApp sends the data JSON serialized string
+    # (see webappbot.html)
+    data = json.loads(update.effective_message.web_app_data.data)
+    await update.message.reply_html(
+        text=(
+            f"You selected the color with the HEX value <code>{data['hex']}</code>. The "
+            f"corresponding RGB value is <code>{tuple(data['rgb'].values())}</code>."
+        ),
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+def main() -> None:
+    with open('token.txt', 'r') as f:
+        TOKEN = f.read()
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
